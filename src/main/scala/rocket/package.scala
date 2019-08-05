4f2ea3b95935ba9e78990d0101abdcac14bf7f2c
Kamyar Mohajerani <kammoh@gmail.com>
2019-08-05 00:09:09 -0400

    remove implicit Bits->SInt/UInt conversions from package object

--- src/main/scala/rocket/package.scala
+++ src/main/scala/rocket/package.scala
@@ -2,11 +2,4 @@
 
 package freechips.rocketchip
 
-import scala.language.implicitConversions
-import chisel3.{Bits, SInt, UInt}
-
-package object rocket extends rocket.constants.ScalarOpConstants with rocket.constants.MemoryOpConstants{
-
-  implicit def bitsToUInt(b: Bits): UInt = b.asUInt
-  implicit def bitsToSInt(b: Bits): SInt = b.asSInt
-}
+package object rocket extends rocket.constants.ScalarOpConstants with rocket.constants.MemoryOpConstants
